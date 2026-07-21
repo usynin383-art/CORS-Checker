1b758641a432e1a1a38dba4f5f282a8b2ba2107c
Andrey Usynin <usynin383@gmail.com>
2026-07-21 20:35:31 +0500

    chore: install prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,38 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import ts from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  
-  eslintConfigPrettier,
-
+export default [
   {
+    ignores: [".next/*", "node_modules/*", "dist/*"] // Во Flat Config используется ключ ignores вместо ignoredFiles
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": ts.plugin,
+      "@next/next": nextPlugin,
+    },
+    languageOptions: {
+      parser: ts.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      ...ts.configs.recommended.reduce((acc, config) => ({ ...acc, ...config.rules }), {}),
+      
       "no-console": ["warn", { allow: ["warn", "error"] }],
-      
+      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-      
       "@typescript-eslint/no-explicit-any": "error",
-      
       "no-empty": "error"
-    }
-  }
+    },
+  },
+  eslintConfigPrettier,
 ];
-
-export default eslintConfig;
